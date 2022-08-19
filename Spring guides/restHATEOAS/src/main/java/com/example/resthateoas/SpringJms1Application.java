ea8ac1720d54c065347eabc6bc0a377b08990c53
Marcelo <marcelorj.oliveira@gmail.com>
2022-08-19 12:26:13 -0300

    Beginning restHATEOAS

---
+++ Spring guides/restHATEOAS/src/main/java/com/example/resthateoas/SpringJms1Application.java
@@ -0,0 +1,13 @@
+package com.example.resthateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJms1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJms1Application.class, args);
+	}
+
+}
