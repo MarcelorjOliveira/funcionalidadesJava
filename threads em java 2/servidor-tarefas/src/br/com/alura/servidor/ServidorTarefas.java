30ad4fae572958c446a3c1249c7caddcdcfa75dd
Marcelo <marcelorj.oliveira@gmail.com>
2022-08-23 11:09:49 -0300

    Finalizar thread parte 2

--- threads em java 2/servidor-tarefas/src/br/com/alura/servidor/ServidorTarefas.java
+++ threads em java 2/servidor-tarefas/src/br/com/alura/servidor/ServidorTarefas.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -13,20 +15,30 @@ public class ServidorTarefas {
 	private ExecutorService threadPool;
 	private ServerSocket servidor;
 	private AtomicBoolean estaRodando;
+	private BlockingQueue<String> filaComandos;
 
 	public ServidorTarefas() throws IOException {
 		System.out.println("Iniciando servidor");
 		this.servidor = new ServerSocket(12345);
-		this.threadPool = Executors.newFixedThreadPool(4); //newCachedThreadPool();
+		this.threadPool = Executors.newCachedThreadPool(); //newFixedThreadPool(4, new FabricaDeThreads() ); //;
 		this.estaRodando = new AtomicBoolean(true);
+		this.filaComandos = new ArrayBlockingQueue<>(2);
+		iniciarConsumidores();
+	}
+
+	private void iniciarConsumidores() {
+		int qtdConsumidores = 2;
+		for(int i=0; i < qtdConsumidores; i++) {
+			TarefaConsumir tarefa = new TarefaConsumir(filaComandos);
+			this.threadPool.execute(tarefa);
+			
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
-
 		ServidorTarefas servidor = new ServidorTarefas();
 		servidor.rodar();
 		servidor.parar();
-
 	}
 
 	public void parar() throws IOException {
@@ -41,7 +53,7 @@ public class ServidorTarefas {
 				Socket socket = servidor.accept();
 				System.out.println("Aceitando novo cliente na porta " + socket.getPort());
 	
-				DistribuirTarefas distribuirTarefas = new DistribuirTarefas(threadPool, socket, this);
+				DistribuirTarefas distribuirTarefas = new DistribuirTarefas(threadPool, filaComandos, socket, this);
 				Thread threadCliente = new Thread(distribuirTarefas);
 				threadCliente.start();
 			} catch(SocketException e) {
